8a459ffe799b73f454565f5a60bbe50e0569a8fa
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-21 15:24:45 -0700

    Use directories-next to locate config dir

--- src/bin/graphene.rs
+++ src/bin/graphene.rs
@@ -1,6 +1,13 @@
-use clap::*;
+use anyhow::anyhow;
+use clap::{crate_description, crate_name, crate_version, App, AppSettings};
+use directories_next::ProjectDirs;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    let project_dirs = ProjectDirs::from("", "", "graphene-cli")
+        .ok_or(anyhow!("couldn't find home directory path"))?;
+    let config_dir = project_dirs.config_dir();
+    println!("{:?}", config_dir);
+
     App::new(crate_name!())
         .about(crate_description!())
         // TODO: clap doesn't add a newline to version output
@@ -10,4 +17,6 @@ fn main() {
         .global_setting(AppSettings::VersionlessSubcommands)
         .global_setting(AppSettings::DisableHelpSubcommand)
         .get_matches();
+
+    Ok(())
 }
