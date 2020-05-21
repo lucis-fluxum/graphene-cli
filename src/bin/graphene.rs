d40768a92e998ea8876153e262eccb5b61f566c9
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-21 11:31:22 -0700

    Create basic clap app

--- src/bin/graphene.rs
+++ src/bin/graphene.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use clap::*;
+
+fn main() {
+    App::new(crate_name!())
+        // TODO: clap doesn't add a newline to version output
+        .version(format!("{}\n", crate_version!()).as_str())
+        .get_matches();
+}
