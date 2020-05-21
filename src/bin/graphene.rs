d69a111a0a43279afa70f8711039ad5e1b905ce4
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-21 15:00:03 -0700

    Customize main App a bit more

--- src/bin/graphene.rs
+++ src/bin/graphene.rs
@@ -2,7 +2,12 @@ use clap::*;
 
 fn main() {
     App::new(crate_name!())
+        .about(crate_description!())
         // TODO: clap doesn't add a newline to version output
         .version(format!("{}\n", crate_version!()).as_str())
+        .global_setting(AppSettings::ColoredHelp)
+        .global_setting(AppSettings::SubcommandRequiredElseHelp)
+        .global_setting(AppSettings::VersionlessSubcommands)
+        .global_setting(AppSettings::DisableHelpSubcommand)
         .get_matches();
 }
