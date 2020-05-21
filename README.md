59bbb447d33d4edb4b3b1a4abb310804c7530ad7
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-21 11:05:55 -0700

    Switch to clap since abscissa is so complicated

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # graphene-cli
 
 A command-line client for the GrapheneDB API.
-
-This application is authored using [Abscissa], a Rust application framework.
-
-[Abscissa]: https://github.com/iqlusioninc/abscissa
--- src/bin/graphene.rs
+++ src/bin/graphene.rs
@@ -1,11 +1,1 @@
-//! Main entry point for GrapheneCli
-
-#![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
-#![forbid(unsafe_code)]
-
-use graphene_cli::application::APPLICATION;
-
-/// Boot GrapheneCli
-fn main() {
-    abscissa_core::boot(&APPLICATION);
-}
+fn main() {}
