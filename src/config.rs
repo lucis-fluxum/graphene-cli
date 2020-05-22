e861b44e962427931bd612e5a4661fb3f04d07da
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-22 15:39:40 -0700

    Add some log messages
    [skip ci]

--- src/config.rs
+++ src/config.rs
@@ -9,9 +9,11 @@ pub struct Config {
 
 pub fn get_or_create_config(config_path: &Path) -> anyhow::Result<Config> {
     let config: Config = if config_path.exists() {
+        log::debug!("loading {}", config_path.to_string_lossy());
         toml::from_str(&fs::read_to_string(config_path).context("couldn't read config file")?)
             .context("couldn't parse config file")?
     } else {
+        log::debug!("no config found, creating empty one");
         fs::create_dir_all(config_path.parent().context("invalid config dir")?)
             .context("couldn't create config directory")?;
         fs::File::create(config_path).context("couldn't create config file")?;
