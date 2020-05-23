ac8c8d5df77adcbe4769dd652f625ece7a20e6ba
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-23 14:14:02 -0700

    Use serde(rename = ...) on several model fields

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
 
 // TODO: Use more accurate types for these fields, like uuid, url, chrono date times, etc.
-// TODO: Use serde(rename = "...") using field names in API documentation
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Cluster {
+    #[serde(rename = "masterURL")]
     master_url: String,
     nodes: Vec<ServerId>,
 }
@@ -20,15 +20,22 @@ pub struct Db {
     id: String,
     name: String,
     url: String,
+    #[serde(rename = "boltURL")]
     bolt_url: String,
+    #[serde(rename = "webAdminURL")]
     web_admin_url: String,
+    #[serde(rename = "metricsURL")]
     metrics_url: Option<String>,
+    #[serde(rename = "createdAt")]
     created_at: String,
     version: String,
+    #[serde(rename = "currentSize")]
     current_size: i64,
     plugins: Vec<Plugin>,
+    #[serde(rename = "maxSize")]
     max_size: i64,
     plan: Plan,
+    #[serde(rename = "awsRegion")]
     aws_region: String,
     counters: Counters,
     cluster: Cluster,
@@ -42,6 +49,7 @@ pub struct Limits {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Plan {
+    #[serde(rename = "type")]
     plan_type: String,
     limits: Limits,
 }
@@ -51,12 +59,15 @@ pub struct Plugin {
     id: String,
     name: String,
     enabled: bool,
+    #[serde(rename = "isManaged")]
     is_managed: bool,
     kind: Option<String>,
+    #[serde(rename = "createdAt")]
     created_at: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ServerId {
+    #[serde(rename = "serverId")]
     server_id: i64,
 }
