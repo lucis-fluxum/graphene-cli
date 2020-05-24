fe9db91136a337b1056a77e54eb0c7792b61f685
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-23 17:44:29 -0700

    Extract API root into a constant

--- src/api.rs
+++ src/api.rs
@@ -4,8 +4,10 @@ use reqwest::{Client, RequestBuilder};
 
 pub mod db;
 
+const API_ROOT: &'static str = "https://api.graphenedb.com/v1";
+
 pub(crate) fn get(client: &Client, config: &Config, path: &str) -> Result<RequestBuilder> {
     Ok(client
-        .get(&format!("https://api.graphenedb.com/v1/{}", path))
+        .get(&format!("{}/{}", API_ROOT, path))
         .header("api_key", config.api_key()))
 }
